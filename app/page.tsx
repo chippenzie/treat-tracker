b664d0302c10d7d83d4c131b0e2c0dfb40863973
Chip Harlan <chip.harlan@gmail.com>
2024-04-29 13:51:15 -0400

    testing vercel updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,20 @@ import { auth } from "@/auth"
 
 export default async function Index() {
   const session = await auth();
+  
+
+  const thisInterval = Math.floor(Number(new Date()) / (1000 * 30))
+
+  /*
+  const dayOfYear = (date: any) => {
+    const yearDate:any = new Date(date.getFullYear(), 0, 0);
+    // period - 1 year
+    //return  date.getFullYear() + ':' + Math.floor((date - yearDate) / (1000 * 60 * 60 * 24 ));
+    // period - 30 seconds
+    return  date.getFullYear() + ':' + Math.floor((date) / (1000 * 30 ));
+  }
+  */
+
 
   return (
     <div className="flex flex-col gap-6">
@@ -12,6 +26,9 @@ export default async function Index() {
         </div>
         <pre className="py-6 px-4 whitespace-pre-wrap break-all">
           {JSON.stringify(session, null, 2)}
+        </pre>
+        <pre className="py-6 px-4 whitespace-pre-wrap break-all">
+          {JSON.stringify(thisInterval, null, 2)}
         </pre>
       </div>
     </div>
